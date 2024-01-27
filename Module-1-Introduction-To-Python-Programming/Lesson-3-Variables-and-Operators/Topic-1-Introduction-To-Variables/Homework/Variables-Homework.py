8133ee20a635ab966821d05789f66460a497e52b
Saint. Clever <parchment@gmx.com>
2024-01-27 13:41:37 -0500

    Update variable hw

--- Module-1-Introduction-To-Python-Programming/Lesson-3-Variables-and-Operators/Topic-1-Introduction-To-Variables/Homework/Variables-Homework.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-3-Variables-and-Operators/Topic-1-Introduction-To-Variables/Homework/Variables-Homework.py
@@ -3,3 +3,12 @@
 # - Declares a string variable `favorite_food`, an integer variable `quantity`, and a float variable `price`.
 # - Changes `favorite_food` to a different food item.
 # - Prints all variables before and after the change.
+
+favorite_food = "bread"
+quantity = 15
+price = 25.0
+
+print(favorite_food, quantity, price)
+
+favorite_food = "rice"
+print(favorite_food, quantity, price)
