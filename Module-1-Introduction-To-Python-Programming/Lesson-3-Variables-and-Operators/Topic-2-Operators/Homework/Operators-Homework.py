c7ddb00313ea42137ef0dae90962cac98a2fdc36
Saint. Clever <parchment@gmx.com>
2024-01-27 14:16:21 -0500

    variables and operators  assignment and hw update

--- Module-1-Introduction-To-Python-Programming/Lesson-3-Variables-and-Operators/Topic-2-Operators/Homework/Operators-Homework.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-3-Variables-and-Operators/Topic-2-Operators/Homework/Operators-Homework.py
@@ -1,5 +1,12 @@
 ## Homework
 # Write a Python script that:
 # - Calculates and prints the result of `(50 - 5*6) / 4`.
+print((50 - 5 * 6) / 4)
+
+
 # - Checks if `100` is not equal to `90` and prints the result.
+print(100 != 90)
+
+
 # - Uses `and`, `or`, and `not` to evaluate `((5 > 3) and (10 < 12)) or not(15 < 20)` and prints the outcome.
+print(((5 > 3) and (10 < 12)) or not (15 < 20))
