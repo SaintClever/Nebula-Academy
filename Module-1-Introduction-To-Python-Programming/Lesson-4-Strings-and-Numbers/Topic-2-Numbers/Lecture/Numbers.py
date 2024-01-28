a3b9ffb7e868ca5ceb7568d4ed87070cc2244193
Saint. Clever <parchment@gmx.com>
2024-01-28 13:25:36 -0500

    Remove comments

--- Module-1-Introduction-To-Python-Programming/Lesson-4-Strings-and-Numbers/Topic-2-Numbers/Lecture/Numbers.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-4-Strings-and-Numbers/Topic-2-Numbers/Lecture/Numbers.py
@@ -1,29 +1,29 @@
-# # Practice Assignments
-# # Basic Arithmetic Operations:
-# import math
+# Practice Assignments
+# Basic Arithmetic Operations:
+import math
 
-# # Perform addition, subtraction, multiplication, and division with integers and floats. Print the results.
-# # Using Built-In Functions:
-# print(1 + 1)
-# print(8674309 - 1000)
-# print("Hello " * 3)
-# print(100 / 3.5)
-# print()
+# Perform addition, subtraction, multiplication, and division with integers and floats. Print the results.
+# Using Built-In Functions:
+print(1 + 1)
+print(8674309 - 1000)
+print("Hello " * 3)
+print(100 / 3.5)
+print()
 
-# # Find the absolute value, round off a float, and calculate the power of numbers using built-in functions.
-# # Experiment with the Math Module:
-# print(abs(-3.14))
-# print(round(100 / 3.5))
-# print(pow(5, 2))  # no need for math import
-# print(5**5)
-# print()
+# Find the absolute value, round off a float, and calculate the power of numbers using built-in functions.
+# Experiment with the Math Module:
+print(abs(-3.14))
+print(round(100 / 3.5))
+print(pow(5, 2))  # no need for math import
+print(5**5)
+print()
 
-# # Use at least three functions from the math module, such as math.sqrt(), math.pi, and math.pow(), and print their results.
-# # Homework Assignments
-# # Calculator Program:
-# print(math.sqrt(16))
-# print(math.pi)
-# print(math.pow(5, 2))
+# Use at least three functions from the math module, such as math.sqrt(), math.pi, and math.pow(), and print their results.
+# Homework Assignments
+# Calculator Program:
+print(math.sqrt(16))
+print(math.pi)
+print(math.pow(5, 2))
 
 
 # Write a simple calculator that asks the user for two numbers and an operation (add, subtract, multiply, divide) and then prints the result.
@@ -56,45 +56,45 @@ def calculator():
 calculator()
 
 
-# # Create a script that generates and prints 5 random integers between 1 and 100.
-# # Infinite and NaN (Not a Number) Concepts:
+# Create a script that generates and prints 5 random integers between 1 and 100.
+# Infinite and NaN (Not a Number) Concepts:
 
-# import random
+import random
 
-# print(random.sample(range(1, 100), 5))
+print(random.sample(range(1, 100), 5))
 
-# # or
+# or
 
-# result = []
-# while len(result) != 5:
-#     result.append(random.randint(2, 99))
-# print(result)
-# print()
-
-
-# # Write a script demonstrating the use of infinity and NaN in Python. Show how to test for these values in a variable.
-
-# import math
-
-# # Nan
-# print(float("nan"), math.isnan(float("nan")))
-
-# if math.isnan(float("nan")):
-#     print("NaN")
-# else:
-#     print("It's just Nanna or a number")
-# print()
+result = []
+while len(result) != 5:
+    result.append(random.randint(2, 99))
+print(result)
+print()
 
 
-# # infinity
-# print(float("inf"), float("-inf"))
-# print(math.inf, -math.inf)
+# Write a script demonstrating the use of infinity and NaN in Python. Show how to test for these values in a variable.
+
+import math
+
+# Nan
+print(float("nan"), math.isnan(float("nan")))
+
+if math.isnan(float("nan")):
+    print("NaN")
+else:
+    print("It's just Nanna or a number")
+print()
 
 
-# jeff_bezos_bank_account = 100_000_000_000_000_000_000
-# infinite_universe = math.inf
+# infinity
+print(float("inf"), float("-inf"))
+print(math.inf, -math.inf)
 
-# if infinite_universe > jeff_bezos_bank_account:
-#     print("I guess Bezos isn't that rich")
-# else:
-#     print("seriously... -___-")
+
+jeff_bezos_bank_account = 100_000_000_000_000_000_000
+infinite_universe = math.inf
+
+if infinite_universe > jeff_bezos_bank_account:
+    print("I guess Bezos isn't that rich")
+else:
+    print("seriously... -___-")
