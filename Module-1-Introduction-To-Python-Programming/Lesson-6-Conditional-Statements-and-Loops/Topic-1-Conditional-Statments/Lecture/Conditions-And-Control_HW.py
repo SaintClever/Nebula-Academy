9dcb702edad840473b4409e84f0c8f63bc4d238e
Saint. Clever <parchment@gmx.com>
2024-02-03 17:08:36 -0500

    Update decimal count

--- Module-1-Introduction-To-Python-Programming/Lesson-6-Conditional-Statements-and-Loops/Topic-1-Conditional-Statments/Lecture/Conditions-And-Control_HW.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-6-Conditional-Statements-and-Loops/Topic-1-Conditional-Statments/Lecture/Conditions-And-Control_HW.py
@@ -10,16 +10,16 @@ temp = input("Temp ex: 32F or 100C: ").lower()
 
 if temp[-1] in ["c", "f"]:
     int_temp = int(temp[:-1])
-    celsius = (int_temp - 32) * 5 / 9
-    fahrenheit = (int_temp * 9 / 5) + 32
 
     if "f" in temp:
-        print(f"{celsius}°C")
+        celsius = (int_temp - 32) * 5 / 9
+        print(f"{celsius:.2f}°C")
     elif "c" in temp:
-        print(f"{fahrenheit}°F")
+        fahrenheit = (int_temp * 9 / 5) + 32
+        print(f"{fahrenheit:.2f}°F")
 else:
     print("Try adding F or C, ex: 35f")
-
+print()
 
 # Grading System:
 # Implement a grading system based on scores: A (90-100), B (80-89), C (70-79), D (60-69), F (<60).
