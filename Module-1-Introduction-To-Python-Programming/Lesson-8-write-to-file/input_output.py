3c994a03752950558ca07c4338d6e2d54480da0b
Saint. Clever <parchment@gmx.com>
2024-02-10 20:15:04 -0500

    update I/O homework

--- Module-1-Introduction-To-Python-Programming/Lesson-8-write-to-file/input_output.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-8-write-to-file/input_output.py
@@ -1,7 +1,7 @@
 import os
 
-#### Practice Exercises
-# 1. Create a Python script to write and read a list of your favorite movies to a file.
+# #### Practice Exercises
+# # 1. Create a Python script to write and read a list of your favorite movies to a file.
 
 while True:
     user_movie = input("Favorite movie: ")
@@ -15,21 +15,29 @@ while True:
             break
 
 
-# 2. Write a script to check if a file named `notes.txt` exists. If it does, append a new note; if not, create and add a note.
-if not os.path.isfile("notes.txt"):
-    print("notes.txt is nonexistent")
-else:
-    print("notes.txt exist")
+# # 2. Write a script to check if a file named `notes.txt` exists. If it does, append a new note; if not, create and add a note.
+if os.path.isfile("notes.txt"):
     with open("notes.txt", "a") as note:
         note.write("new note added\n")
+else:
+    with open("notes.txt", "w") as note:
+        note.write("note added\n")
 
 
-#### Homework
-# 1. Develop a Python script that reads a file and counts the number of words in it.
+# #### Homework
+# # 1. Develop a Python script that reads a file and counts the number of words in it.
 with open("favorite_movies.txt", "r") as movie:
     print(len("".join(movie.readlines()).split()))
 
 
 # 2. Create a program that lists all the files in a given directory and prints their file sizes.
-user_path = os.path.expanduser(input("Paste directory: "))
-print(f"\n{os.listdir(user_path)}")
+for dir_path, dir_names, files in os.walk(input("Directory: ")):
+    for file in files:
+        file_path = os.path.join(f"{dir_path}/{file}")
+
+        try:
+            print(
+                f"\n{file}\n{os.stat(file_path).st_size} Bytes\n{os.stat(file_path).st_size / (1024**2):.2f} MB"
+            )
+        except FileNotFoundError:
+            pass
