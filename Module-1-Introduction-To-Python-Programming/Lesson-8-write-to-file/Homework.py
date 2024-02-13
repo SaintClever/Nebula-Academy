86f1be5294f2b255505b776eb11dc91327af6c4d
Saint. Clever <parchment@gmx.com>
2024-02-12 22:43:47 -0500

    Homework update

--- Module-1-Introduction-To-Python-Programming/Lesson-8-write-to-file/Homework.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-8-write-to-file/Homework.py
@@ -1,3 +1,5 @@
+import os
+
 # ### 1. Simple Text Editor
 # Create a Python script that acts as a simple text editor. The script should allow the user to create a new text file, write content to it, and save it. It should also allow reading and displaying the content of an existing text file.
 
@@ -7,14 +9,14 @@
 # ```
 
 
-# with open("text_editor.txt", "a") as text_editor:
-#     user_input = input("Text Editor (r to read note): ")
+with open("text_editor.txt", "a") as text_editor:
+    user_input = input("Text Editor (r to read note): ")
 
-#     if user_input == "r":
-#         with open("text_editor.txt") as text_editor:
-#             print(text_editor.read())
-#     else:
-#         text_editor.write(f"{user_input}\n")
+    if user_input == "r":
+        with open("text_editor.txt") as text_editor:
+            print(text_editor.read())
+    else:
+        text_editor.write(f"{user_input}\n")
 
 
 # ### 2. Log File Summary
@@ -24,9 +26,9 @@
 # # Hint: Open the log file in read mode, use a loop to count the lines, and then print the count.
 # ```
 
-# with open("text_editor.txt", "r") as text_editor:
-#     for i, line in enumerate(text_editor.readlines()):
-#         print(f'{i+1}: {line.replace("\n", "")}')
+with open("text_editor.txt", "r") as text_editor:
+    for i, line in enumerate(text_editor.readlines()):
+        print(f'{i+1}: {line.replace("\n", "")}')
 
 
 # ### 3. Update a Text File
@@ -36,21 +38,29 @@
 # # Hint: Read the file's content into a list, update the specific line, then write the list back to the file.
 # ```
 
-data = []
+# data = []
+data = {}
 
 with open("text_editor.txt", "r") as text_editor:
     print("What line to replace:\n")
 
     for i, line in enumerate(text_editor.readlines()):
         print(f'{i}: {line.replace("\n", "")}')
-        data.append(line.replace("\n", ""))
+        # data.append(line.replace("\n", ""))
+        data[i] = line.replace("\n", "")
 
 with open("text_editor.txt", "w") as text_editor:
     line_number = int(input("\nChoose line number: "))
     data[line_number] = input(f"New text for {line_number}: ")
+    print("")
 
-    for new_line in data:
-        text_editor.write(f"{new_line}\n")
+    # for i, new_line in data:
+    #     print(f"\n{i}: {new_line}")
+    #     text_editor.write(f"{new_line}")
+
+    for key, value in data.items():
+        print(f"{key}: {value}")
+        text_editor.write(f"{value}\n")
 
 
 # ### 4. List and Count Words in a Directory
@@ -59,3 +69,12 @@ with open("text_editor.txt", "w") as text_editor:
 # ```python
 # # Hint: Use 'os.listdir()' to find `.txt` files, then open each file, read its contents, and count the words.
 # ```
+
+for dir_path, dir_names, files in os.walk(input("Directory: ")):
+    for file in files:
+        if ".txt" in file:
+            with open(file, "r") as read_file:
+                print(f"{file}: Word Count - {len(read_file.read().replace('\n', ''))}")
+
+            with open(file, "r") as read_file:
+                print(read_file.read() + "\n")
