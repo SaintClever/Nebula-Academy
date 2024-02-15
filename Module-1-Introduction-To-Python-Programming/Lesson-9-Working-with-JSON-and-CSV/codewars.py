c5ac8d281ac1f76a5d607b1e324cb8657c7ae596
Saint. Clever <parchment@gmx.com>
2024-02-14 20:24:04 -0500

    Refactor code

--- Module-1-Introduction-To-Python-Programming/Lesson-9-Working-with-JSON-and-CSV/codewars.py
+++ Module-1-Introduction-To-Python-Programming/Lesson-9-Working-with-JSON-and-CSV/codewars.py
@@ -4,20 +4,26 @@ output -> int
 consider "a, e, i, o, u", lowercase and /or spaces
 """
 
+# def get_count(sentence: str) -> int:
+#     vowels: list = ["a", "e", "i", "o", "u"]
+#     count = 0
+
+#     for letter in sentence:
+#         if letter in vowels:
+#             count += 1
+
+#     return count
+
+
+# print(get_count("abracadabra"))
+
 
 def get_count(sentence: str) -> int:
     vowels: list = ["a", "e", "i", "o", "u"]
-    count = 0
-
-    for letter in sentence:
-        if letter in vowels:
-            count += 1
-
-    return count
+    return len("".join(letter for letter in sentence if letter in vowels))
 
 
 print(get_count("abracadabra"))
-
 
 """
 input -> str
@@ -26,18 +32,27 @@ remove vowels
 """
 
 
+# def disemvowel(string_: str) -> str:
+#     vowels: list = ["a", "e", "i", "o", "u"]
+#     vowels_upper_case: list = " ".join(vowels).upper().split()
+#     sentence: str = ""
+
+#     for letter in string_:
+#         if letter in vowels + vowels_upper_case:
+#             list(string_).remove(letter)
+#         else:
+#             sentence += letter
+
+#     return sentence
+
+
+# print(disemvowel("This website is for losers LOL!"))
+# # "Ths wbst s fr lsrs LL!"
+
+
 def disemvowel(string_: str) -> str:
-    vowels: list = ["a", "e", "i", "o", "u"]
-    vowels_upper_case: list = " ".join(vowels).upper().split()
-    sentence: str = ""
-
-    for letter in string_:
-        if letter in vowels + vowels_upper_case:
-            list(string_).remove(letter)
-        else:
-            sentence += letter
-
-    return sentence
+    vowels: str = "aeiouAEIOU"
+    return "".join(letter for letter in string_ if letter not in vowels)
 
 
 print(disemvowel("This website is for losers LOL!"))
