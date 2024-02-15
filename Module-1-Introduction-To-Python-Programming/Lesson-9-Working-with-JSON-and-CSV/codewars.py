4ffccab3e34ebf580f1dc8d6886ae87775b0c527
Saint. Clever <parchment@gmx.com>
2024-02-14 19:45:33 -0500

    Codewars practice

---
+++ Module-1-Introduction-To-Python-Programming/Lesson-9-Working-with-JSON-and-CSV/codewars.py
@@ -0,0 +1,44 @@
+"""
+input -> str
+output -> int
+consider "a, e, i, o, u", lowercase and /or spaces
+"""
+
+
+def get_count(sentence: str) -> int:
+    vowels: list = ["a", "e", "i", "o", "u"]
+    count = 0
+
+    for letter in sentence:
+        if letter in vowels:
+            count += 1
+
+    return count
+
+
+print(get_count("abracadabra"))
+
+
+"""
+input -> str
+output -> str
+remove vowels
+"""
+
+
+def disemvowel(string_: str) -> str:
+    vowels: list = ["a", "e", "i", "o", "u"]
+    vowels_upper_case: list = " ".join(vowels).upper().split()
+    sentence: str = ""
+
+    for letter in string_:
+        if letter in vowels or letter in vowels_upper_case:
+            list(string_).remove(letter)
+        else:
+            sentence += letter
+
+    return sentence
+
+
+print(disemvowel("This website is for losers LOL!"))
+# "Ths wbst s fr lsrs LL!"
