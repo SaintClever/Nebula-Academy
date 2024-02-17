cfd9a90f59d2c0cf96841df9a4621fcc8a8918ce
Saint. Clever <parchment@gmx.com>
2024-02-17 14:12:29 -0500

    Update folder structure and add CSV assignment and lecture

---
+++ Module-1-Introduction-To-Python-Programming/Lesson-9-Working-with-JSON-and-CSV/JSON/test_00.py
@@ -0,0 +1,23 @@
+import json
+
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+]
+
+data_str: str = json.dumps(db, indent=4)
+# print(data_str)
+
+data_list: list = json.load(open("test.json"))
+# print(data_list)
+
+
+for data in data_list:
+    print(data)
