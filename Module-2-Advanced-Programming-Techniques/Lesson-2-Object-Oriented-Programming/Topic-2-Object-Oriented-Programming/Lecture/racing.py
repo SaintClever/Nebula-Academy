82637e6f455a13063ca9a3f6eb213cf8439446a0
Saint. Clever <parchment@gmx.com>
2024-02-28 09:23:47 -0500

    Add race functional feature

--- Module-2-Advanced-Programming-Techniques/Lesson-2-Object-Oriented-Programming/Topic-2-Object-Oriented-Programming/Lecture/racing.py
+++ Module-2-Advanced-Programming-Techniques/Lesson-2-Object-Oriented-Programming/Topic-2-Object-Oriented-Programming/Lecture/racing.py
@@ -1,3 +1,5 @@
+import random
+
 # ## Vehicle Racing Simulation
 
 # ### Context
@@ -12,6 +14,13 @@
 # Start by defining your class with the `__init__` method to initialize the attributes. For the `move` method, consider how you might simulate movement based on the vehicle's speed. The `breakdown` method could randomly affect the vehicle based on its reliability.
 
 
+class Weapon:
+    # def __init__(self): ...
+
+    def missile(self):
+        return "Missiles launched: \U0001F680\U0001F680\U0001F680"
+
+
 class Vechicle:
     def __init__(self, name, speed, reliability):
         self.name = name
@@ -19,13 +28,15 @@ class Vechicle:
         self.reliability = reliability
 
     def move(self, drivers_speed: int = 0):
-        if drivers_speed > 160:
+        if drivers_speed > 180:
             self.reliability -= 10
             return f"Your {self.name} is going too FAST!!! Your reliability rate is decreasing! It's now {self.reliability}"
-
-        elif drivers_speed < 80 and drivers_speed > 65:
+        elif drivers_speed < 95:
             self.speed += 10
-            return f"Pick up the pace! Now your current speed is {self.speed}! wow!"
+            return f"Pick up the pace! Now your current speed is now {self.speed}! wow!"
+        elif drivers_speed <= 150 and drivers_speed > 100:
+            # this the key words "finish line" to trigger winner
+            return f"Your're at the finish line! GO GO!"
         else:
             return f"Are you sleeping at the wheel?!?!"
 
@@ -54,11 +65,11 @@ class Vechicle:
 
 
 class Car(Vechicle):
-    def __init__(self):
-        super().__init__("Mazda: RX-7", 120, 4.0)
+    def __init__(self, name, speed, reliability):
+        super().__init__(name, speed, reliability)
 
 
-mazda = Car()
+mazda = Car("\U0001F3CE  Mazda: RX-7", 75, 4.0)
 print(mazda.name)
 print(mazda.move(180))
 print(mazda.breakdown(3))
@@ -66,42 +77,71 @@ print()
 
 
 class Motorcycle(Vechicle):
-    def __init__(self):
-        super().__init__("Yamaha: R1", 182, 4.5)
+    def __init__(self, name, speed, reliability):
+        super().__init__(name, speed, reliability)
 
     def move(self, drivers_speed: int = 0):
         if drivers_speed > 160:
             self.reliability -= 10
             return f"Your {self.name} is going too FAST!!! Your reliability rate is decreasing! It's now {self.reliability}"
         elif drivers_speed < 60:
-            return "No time to while on the track!"
+            return "No time to wheelie on the track!"
+        else:
+            return (
+                f"Keep that same steady pace kido. Your current speed is {self.speed}"
+            )
 
 
-yamaha = Motorcycle()
+yamaha = Motorcycle("\U0001F3CD Yamaha: R1", 112, 4.5)
 print(yamaha.name)
 print(yamaha.move(6))
 print(yamaha.breakdown(4))
 print()
 
 
-class Truck(Vechicle):
-    def __init__(self):
-        super().__init__("Tesla: Cybertruck", 130, 5.0)
+class Truck(Vechicle, Weapon):
+    def __init__(self, name, speed, reliability):
+        super().__init__(name, speed, reliability)
+        super().missile()
 
     def ram(self):
         return "Breaking thru the finish line Elon!"
 
 
-tesla = Truck()
+tesla = Truck("\U0001F6FB  Tesla: Cybertruck", 85, 5.0)
 print(tesla.name)
 print(tesla.move(120))
 print(tesla.breakdown(4))
 print(tesla.ram())
+print(tesla.missile())
 print()
 
 
 # ### Problem 3: Implement the Race Function
 # **Task**: Implement a `race` function where instances of these vehicles compete on a track of a specified distance. The race continues in a loop until one vehicle crosses the finish line, considering potential breakdowns affecting the outcome.
 
+
 # #### Hint
 # Your `race` function will need to keep track of each vehicle's progress. Use a loop to simulate each "tick" of the race, where vehicles have the chance to move forward or experience a breakdown. Keep track of each vehicle's distance covered and compare it to the total race distance to determine when a vehicle has won the race. Remember to factor in the possibility of a vehicle breaking down, which could affect its ability to finish.
+
+
+def race(racers):
+
+    for racer, status in racers.items():
+        if "finish line" in status:
+            return print(f"WINNER {racer}!!!")
+        else:
+            return print("Pick up the speed!!!")
+
+
+race(
+    {
+        "mazda": mazda.move(random.randint(0, 200)),
+        "yamaha": yamaha.move(random.randint(0, 200)),
+        "tesla": tesla.move(random.randint(0, 200)),
+    }
+)
+
+# mazda.breakdown(float(random.randint(0, 5))),
+# yamaha.breakdown(float(random.randint(0, 5))),
+# tesla.breakdown(float(random.randint(0, 5))),
