fa7dfa1effaea2e218a326409f0b9dae4a25f995
Saint. Clever <parchment@gmx.com>
2024-03-09 12:09:50 -0500

    Add codewars challenge

---
+++ Module-2-Advanced-Programming-Techniques/Lesson-2-Object-Oriented-Programming/Topic-2-Object-Oriented-Programming/OOP_practice/code_wars_01.py
@@ -0,0 +1,20 @@
+# Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+def spin_words(sentence) -> str:
+    # new_sentence = ""
+
+    # for word in sentence.split():
+    #     if len(word) >= 5:
+    #         new_sentence += word[::-1] + " "
+    #     else:
+    #         new_sentence += word + " "
+
+    # return new_sentence.strip()
+
+    return " ".join(word[::-1] if len(word) >= 5 else word for word in sentence.split())
+
+
+print(spin_words("Hey fellow warriors"))  # --> "Hey wollef sroirraw"
+print(spin_words("This is a test"))  # --> "This is a test"
+print(spin_words("This is another test"))  # --> "This is rehtona test"
