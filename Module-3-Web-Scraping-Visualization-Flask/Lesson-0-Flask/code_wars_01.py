4a7b63bd52059fe1fbf644c83682dc6e01b8a8bb
Saint. Clever <parchment@gmx.com>
2024-03-23 11:53:16 -0400

    Refactor answer

--- Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/code_wars_01.py
+++ Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/code_wars_01.py
@@ -24,7 +24,17 @@ def dont_give_me_five(start, end):
 
     # return len(output)
 
-    return len([i for i in range(start, end + 1) if "5" not in str(i)])
+    # return len([i for i in range(start, end + 1) if "5" not in str(i)])
+
+    # output = 0
+
+    # for i in range(start, end + 1):
+    #     if "5" not in str(i):
+    #         output += 1
+
+    # return output
+
+    return sum(1 for i in range(start, end + 1) if "5" not in str(i))
 
 
 print(dont_give_me_five(1, 9), 8)
