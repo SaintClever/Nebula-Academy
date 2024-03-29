f0917c39a07425954344b308777d936f539f62ee
Saint. Clever <parchment@gmx.com>
2024-03-29 16:54:10 -0400

    Create POST and PUT

--- Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/flask_postgresql_01/app.py
+++ Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/flask_postgresql_01/app.py
@@ -5,6 +5,20 @@ from psycopg2.extras import RealDictCursor
 app = Flask(__name__)
 
 
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.json
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO users (name, email) VALUES (%s, %s)",
+        (data.get("name"), data.get("email")),
+    )
+    conn.commit()
+    cur.close()
+    return jsonify(data), 201
+
+
 @app.route("/users", methods=["GET"])
 def get_users():
     conn = get_db_connection()
@@ -12,7 +26,22 @@ def get_users():
     cur.execute("SELECT * FROM users")
     users = cur.fetchall()
     cur.close()
-    return jsonify(users)
+    return jsonify(users), 200
+
+
+@app.route("/users/<int:id>", methods=["PUT"])
+def update_user(id):
+    data = request.json
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "UPDATE users SET name = %s, email = %s WHERE id = %s",
+        (data.get("name"), data.get("email"), id),
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify(data), 204
 
 
 if __name__ == "__main__":
--- Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/flask_postgresql_01/db.py
+++ Module-3-Web-Scraping-Visualization-Flask/Lesson-0-Flask/flask_postgresql_01/db.py
@@ -1,12 +1,21 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
+
+load_dotenv(".env")
+
+host: str = os.getenv("HOST")
+database: str = os.getenv("DATABASE")
+user: str = os.getenv("USER")
+password: str = os.getenv("PASSWORD")
 
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host="localhost",
-        database="test_db",
-        user="postgres",
-        password="4321PostgreSQL!",
+        host=host,
+        database=database,
+        user=user,
+        password=password,
     )
     return conn
